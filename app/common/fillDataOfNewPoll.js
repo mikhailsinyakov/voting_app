c0d3d8999bb309578eec2f24e38b75b21032de99
Mikhail Sinyakov <mikhail.sinyakov86@gmail.com>
2018-01-14 15:56:00 +0000

    Change href to new created poll

--- app/common/fillDataOfNewPoll.js
+++ app/common/fillDataOfNewPoll.js
@@ -3,7 +3,7 @@
     const id = path.split("/")[2];
     const name = path.split("/")[3];
     const anchor = document.querySelector("#newPollLink");
-    anchor.href = `https://voting-app-mikhailsinyakov.c9users.io/polls/${id}`;
+    anchor.href = `/polls/${id}`;
     anchor.innerHTML = name;
     
 })();
